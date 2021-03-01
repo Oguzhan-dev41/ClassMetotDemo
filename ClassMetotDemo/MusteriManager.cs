bc2fd37eb344764a09f0e9dd29acb28eec8ad9ea
Oğuzhan Kandar <oguzhankandar14@gmail.com>
2021-03-01 20:54:41 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + "Musteri Eklendi ! ");
+        }
+
+        public void List(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + "Musteri Listelendi ! ");
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + "Musteri Silindi !");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+
+            //musteri.Ad = "Oğuzhan";
+            //musteri.Soyad = "Kandar";
+            //musteri.Sehir = "Kocaeli";
+            //musteri.Id = 1;
+
+
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Add(musteri);
+            musteriManager.List(musteri);
+            musteriManager.Delete(musteri);
+        }
+    }
+}
